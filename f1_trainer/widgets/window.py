e802e395d71ab1296aa1c762fa1e9ef4c989c78a
Mrowr Purr <mrowr.purr@gmail.com>
2024-03-29 16:21:58 -0700

    Hook up events, add default DLL, print platform

--- f1_trainer/widgets/window.py
+++ f1_trainer/widgets/window.py
@@ -1,5 +1,13 @@
+import platform
+
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+
+from f1_trainer.lib.dll_injection import inject_dll
+
+DEFAULT_DLL_PATH = (
+    "C:/Code/mrowrpurr/Fallout1_Trainer_Example/build/windows/x86/debug/f1_trainer.dll"
+)
 
 
 class Window(QWidget):
@@ -11,16 +19,21 @@ class Window(QWidget):
 
     def __init__(self) -> None:
         super().__init__()
+
+        print(f"Platform: {platform.architecture()[0]}")
+
         self.setWindowTitle("Modding Framework")
         self.setWindowIcon(QIcon(":/icon.ico"))
         self._widgets()
         self._layout()
+        self._events()
         self.setStyleSheet(
             """
             QLabel { font-size: 36px; font-weight: bold; }
             QWidget { font-size: 24px; }
         """
         )
+        self._txt_dll_path.setText(DEFAULT_DLL_PATH)
 
     def _widgets(self):
         self._lbl_title = QLabel("DLL injection")
@@ -41,8 +54,16 @@ class Window(QWidget):
         layout.addWidget(self._btn_eject)
         layout.addStretch()
 
-    # def _on_click(self) -> None:
-    #     dll_absolute_path = "C:/Code/mrowrpurr/Fallout1_Trainer_Example/build/windows/x86/debug/f1_trainer.dll"
-    #     process_name = "falloutwHR.exe"
-    #     print(f"Injecting DLL: {dll_absolute_path} into process: {process_name}...")
-    #     inject_dll(process_name, dll_absolute_path)
+    def _events(self):
+        self._btn_inject.clicked.connect(self._on_inject)
+        self._btn_eject.clicked.connect(self._on_eject)
+
+    def _on_inject(self) -> None:
+        dll_absolute_path = self._txt_dll_path.text()
+        process_name = self._txt_process_name.text()
+        print(f"Injecting DLL: {dll_absolute_path} into process: {process_name}...")
+        inject_dll(process_name, dll_absolute_path)
+
+    def _on_eject(self) -> None:
+        print("TODO")
+        pass

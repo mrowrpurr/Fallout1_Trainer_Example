7057baeec2b18685ba3b95ae0f93c2d656faf79f
Mrowr Purr <mrowr.purr@gmail.com>
2024-03-29 15:52:28 -0700

    Accept any DLL path and process name

--- f1_trainer/widgets/window.py
+++ f1_trainer/widgets/window.py
@@ -1,28 +1,48 @@
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QPushButton, QVBoxLayout, QWidget
-
-from f1_trainer.lib.dll_injection import inject_dll
+from PySide6.QtWidgets import QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 
 
 class Window(QWidget):
+    _lbl_title: QLabel
+    _txt_process_name: QLineEdit
+    _txt_dll_path: QLineEdit
+    _btn_inject: QPushButton
+    _btn_eject: QPushButton
 
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle("Fallout 1 Trainer")
+        self.setWindowTitle("Modding Framework")
         self.setWindowIcon(QIcon(":/icon.ico"))
+        self._widgets()
+        self._layout()
+        self.setStyleSheet(
+            """
+            QLabel { font-size: 36px; font-weight: bold; }
+            QWidget { font-size: 24px; }
+        """
+        )
 
+    def _widgets(self):
+        self._lbl_title = QLabel("DLL injection")
+        self._txt_process_name = QLineEdit()
+        self._txt_process_name.setPlaceholderText("Process name")
+        self._txt_dll_path = QLineEdit()
+        self._txt_dll_path.setPlaceholderText("DLL path")
+        self._btn_inject = QPushButton("Inject")
+        self._btn_eject = QPushButton("Eject")
+
+    def _layout(self):
         layout = QVBoxLayout()
         self.setLayout(layout)
-
-        # One button!
-        the_button = QPushButton("Inject DLL!")
-        the_button.clicked.connect(self._on_click)
-        layout.addWidget(the_button)
-
+        layout.addWidget(self._lbl_title)
+        layout.addWidget(self._txt_process_name)
+        layout.addWidget(self._txt_dll_path)
+        layout.addWidget(self._btn_inject)
+        layout.addWidget(self._btn_eject)
         layout.addStretch()
 
-    def _on_click(self) -> None:
-        dll_absolute_path = "C:/Code/mrowrpurr/Fallout1_Trainer_Example/build/windows/x86/debug/f1_trainer.dll"
-        process_name = "falloutwHR.exe"
-        print(f"Injecting DLL: {dll_absolute_path} into process: {process_name}...")
-        inject_dll(process_name, dll_absolute_path)
+    # def _on_click(self) -> None:
+    #     dll_absolute_path = "C:/Code/mrowrpurr/Fallout1_Trainer_Example/build/windows/x86/debug/f1_trainer.dll"
+    #     process_name = "falloutwHR.exe"
+    #     print(f"Injecting DLL: {dll_absolute_path} into process: {process_name}...")
+    #     inject_dll(process_name, dll_absolute_path)
